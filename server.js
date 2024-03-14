bd4a7414b263861078394a8bd78105faaee61b3a
The_Dude <93833148+hasanalt@users.noreply.github.com>
2024-03-14 20:27:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const app = express();
 
-const myLimit = process.argv[2] || '100kb';
-console.log('Using limit:', myLimit);
-
-app.use(bodyParser.json({ limit: myLimit }));
+// No limit for body parser
+app.use(bodyParser.json());
 
 app.all('*', function (req, res, next) {
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    // Set CORS headers: allow all origins, methods, and headers
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
